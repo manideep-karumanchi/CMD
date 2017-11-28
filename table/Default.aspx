0479d1bf7e91b92edc68fb328b46a94bbf433b14
manideep-karumanchi <manideep.karumanchi@gmail.com>
2017-11-28 09:27:08 +0530

    Add files via upload

---
+++ table/Default.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TableRole.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Table ID="Table1" runat="server">
+            <asp:TableRow>
+                <asp:TableCell>First Name:</asp:TableCell>
+                <asp:TableCell><asp:TextBox ID="FNTB" runat="server"></asp:TextBox></asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>Last Name:</asp:TableCell>
+                <asp:TableCell><asp:TextBox ID="LNTB" runat="server"></asp:TextBox></asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>Contact Number</asp:TableCell>
+                <asp:TableCell><asp:TextBox ID="CNTB" runat="server"></asp:TextBox></asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>Contact Type</asp:TableCell>
+                <asp:TableCell>
+                    <asp:DropDownList ID="CTDL" runat="server">
+                        <asp:ListItem Value="Family">Family</asp:ListItem>
+                        <asp:ListItem Value="Friends">Friends</asp:ListItem>
+                        <asp:ListItem Value="Others">Others</asp:ListItem>
+                    </asp:DropDownList>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell ColumnSpan="2"><asp:Button OnClick="Submit_Click" ID="Submit" runat="server" Text="Insert Record" /></asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+    </div>
+        <hr />
+        <asp:Button ID="DC" runat="server" Text="Display Contacts" OnClick="DC_Click" />
+        <asp:Button ID="Update" runat="server" Text="Update" OnClick="Update_Click" />
+        <asp:GridView ID="ContactsGrid" Visible="false" runat="server"></asp:GridView>
+    </form>
+</body>
+</html>
---
+++ table/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableRole
+{
+    class ContactEntity : TableEntity {
+        public String FirstName { get; set;}
+        public String LastName { get; set; }
+        public ContactEntity() { }
+        public ContactEntity(String fn, String ln, String cn, String ct) {
+            FirstName = fn;
+            LastName = ln;
+            PartitionKey = ct;
+            RowKey = cn;
+        }
+    }
+    public partial class Default : System.Web.UI.Page
+    {
+        CloudTable table;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CloudStorageAccount csa = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+            CloudTableClient client = csa.CreateCloudTableClient();
+            table = client.GetTableReference("contactbook");
+            table.CreateIfNotExists();
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            String fn = FNTB.Text;
+            String ln = LNTB.Text;
+            String cn = CNTB.Text;
+            String ct = CTDL.SelectedValue;
+            ContactEntity ce = new ContactEntity(fn,ln,cn,ct);
+            TableOperation insertOperation = TableOperation.Insert(ce);
+            table.Execute(insertOperation);
+        }
+
+        protected void DC_Click(object sender, EventArgs e)
+        {
+            TableQuery<ContactEntity> query = new TableQuery<ContactEntity>();
+            ContactsGrid.DataSource = table.ExecuteQuery(query);
+            ContactsGrid.DataBind();
+            ContactsGrid.Visible = true;
+        }
+
+        protected void Update_Click(object sender, EventArgs e)
+        {
+            TableOperation retrieveOp = TableOperation.Retrieve<ContactEntity>("Family", "999999999");
+            TableResult retrievedresult = table.Execute(retrieveOp);
+            ContactEntity updaterecord = (ContactEntity)retrievedresult.Result;
+            updaterecord.FirstName = "Mani D";
+            table.Execute(TableOperation.InsertOrReplace(updaterecord));
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
