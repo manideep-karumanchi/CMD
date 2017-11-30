7f93ef28ef8d3556bd385db023b176a666953082
manideep-karumanchi <manideep.karumanchi@gmail.com>
2017-11-30 07:28:27 +0530

    Add files via upload

---
+++ table updated/Default.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TableRole.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Table ID="InsertTable" runat="server">
+            <asp:TableRow>
+                <asp:TableCell>First Name:</asp:TableCell>
+                <asp:TableCell><asp:TextBox ID="FNTB" runat="server"></asp:TextBox></asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>Last Name:</asp:TableCell>
+                <asp:TableCell><asp:TextBox ID="LNTB" runat="server"></asp:TextBox></asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>Contact Number</asp:TableCell>
+                <asp:TableCell><asp:TextBox ID="CNTB" runat="server"></asp:TextBox></asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>Contact Type</asp:TableCell>
+                <asp:TableCell>
+                    <asp:DropDownList ID="CTDL" runat="server">
+                        <asp:ListItem Value="Family">Family</asp:ListItem>
+                        <asp:ListItem Value="Friends">Friends</asp:ListItem>
+                        <asp:ListItem Value="Others">Others</asp:ListItem>
+                    </asp:DropDownList>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell ColumnSpan="2"><asp:Button OnClick="Submit_Click" ID="Submit" runat="server" Text="Insert Record" /></asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+    </div>
+        <div>
+            <fieldset id="editfs" runat="server" visible="false">
+                First Name: <asp:TextBox ID="EFNTB" runat="server"></asp:TextBox><br />
+                Last Name: <asp:TextBox ID="ELNTB" runat="server"></asp:TextBox><br />
+                Contact Number: <asp:TextBox ID="ECNTB" runat="server"></asp:TextBox><br />
+                Contact Type: <asp:TextBox ID="ECTTB" runat="server"></asp:TextBox><br />
+                <asp:Button ID="RB" runat="server" Text="Replace" OnClick="RB_Click" />
+                <asp:Button ID="DB" runat="server" Text="Delete" OnClick="DB_Click" />
+
+            </fieldset>
+        </div>
+        <hr />
+        <asp:GridView ID="ContactsGrid" AutoGenerateColumns="false" runat="server" OnRowCommand="ContactsGrid_RowCommand">
+            <Columns>
+                <asp:BoundField DataField="FirstName" HeaderText="First Name" />
+                <asp:BoundField DataField="LastName" HeaderText="Last Name" />
+                <asp:BoundField DataField="RowKey" HeaderText="Contact Number" />
+                <asp:BoundField DataField="PartitionKey" HeaderText="Contact Type" />
+                <asp:ButtonField ButtonType="Button" Text="Edit" />
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ table updated/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableRole
+{
+    class ContactEntity : TableEntity {
+        public String FirstName { get; set;}
+        public String LastName { get; set; }
+        public ContactEntity() { }
+        public ContactEntity(String fn, String ln, String cn, String ct) {
+            FirstName = fn;
+            LastName = ln;
+            PartitionKey = ct;
+            RowKey = cn;
+        }
+    }
+    public partial class Default : System.Web.UI.Page
+    {
+        CloudTable table;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CloudStorageAccount csa = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+            CloudTableClient client = csa.CreateCloudTableClient();
+            table = client.GetTableReference("contactbook");
+            table.CreateIfNotExists();
+            TableQuery<ContactEntity> query = new TableQuery<ContactEntity>();
+            ContactsGrid.DataSource = table.ExecuteQuery(query);
+            ContactsGrid.DataBind();
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            String fn = FNTB.Text;
+            String ln = LNTB.Text;
+            String cn = CNTB.Text;
+            String ct = CTDL.SelectedValue;
+            ContactEntity ce = new ContactEntity(fn,ln,cn,ct);
+            TableOperation insertOperation = TableOperation.Insert(ce);
+            table.Execute(insertOperation);
+        }
+
+        protected void ContactsGrid_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            InsertTable.Visible = false;
+            ContactsGrid.Visible = false;
+            editfs.Visible = true;
+            int index = Convert.ToInt32(e.CommandArgument);
+            GridViewRow selectedRow = ContactsGrid.Rows[index];
+            EFNTB.Text = selectedRow.Cells[0].Text;
+            ELNTB.Text = selectedRow.Cells[1].Text;
+            ECNTB.Text = selectedRow.Cells[2].Text;
+            ECTTB.Text = selectedRow.Cells[3].Text;   
+        }
+
+        protected void RB_Click(object sender, EventArgs e)
+        {
+            TableOperation retrieveOp = TableOperation.Retrieve<ContactEntity>(ECTTB.Text, ECNTB.Text);
+            TableResult retrievedresult = table.Execute(retrieveOp);
+            ContactEntity updaterecord = (ContactEntity)retrievedresult.Result;
+            updaterecord.FirstName = EFNTB.Text;
+            updaterecord.LastName = ELNTB.Text;
+            table.Execute(TableOperation.InsertOrReplace(updaterecord));
+            Response.Redirect("Default.aspx");
+        }
+
+        protected void DB_Click(object sender, EventArgs e)
+        {
+            TableOperation retrieveOp = TableOperation.Retrieve<ContactEntity>(ECTTB.Text, ECNTB.Text);
+            TableResult retrievedresult = table.Execute(retrieveOp);
+            ContactEntity updaterecord = (ContactEntity)retrievedresult.Result;
+            table.Execute(TableOperation.Delete(updaterecord));
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
