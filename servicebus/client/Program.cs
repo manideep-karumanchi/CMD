5b121678fad716f518780edd2d8d39ff325570ea
manideep-karumanchi <manideep.karumanchi@gmail.com>
2018-01-04 10:00:40 +0530

    Add files via upload

---
+++ servicebus/client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Client
+{
+    [ServiceContract]
+    interface IProblemSolver
+    {
+        [OperationContract]
+        int add(int a, int b);
+    }
+    interface IProblemSolverChannel : IProblemSolver, IClientChannel { }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ch = new ChannelFactory<IProblemSolverChannel>("solverEP");
+            var channel = ch.CreateChannel();
+            Console.WriteLine(channel.add(10, 20));
+            Console.Read();
+        }
+    }
+}
---
+++ servicebus/service/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Service
+{
+    [ServiceContract]
+    interface IProblemSolver {
+        [OperationContract]
+        int add(int a, int b);
+    }
+    class ProblemSolver : IProblemSolver {
+        public int add(int a, int b) {
+            return a + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(ProblemSolver));
+            sh.Open();
+            Console.WriteLine("Press ENTER to stop");
+            Console.Read();
+            sh.Close();
+        }
+    }
+}
