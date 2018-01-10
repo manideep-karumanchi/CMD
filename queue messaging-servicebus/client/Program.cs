7fca0b7aea5eaaef710515fd21ab79049fb15f7d
manideep-karumanchi <manideep.karumanchi@gmail.com>
2018-01-10 11:43:38 +0530

    Add files via upload

---
+++ queue messaging-servicebus/client/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri serviceUri = ServiceBusEnvironment.CreateServiceUri("sb", "beccsea", String.Empty);
+            TokenProvider credentials = TokenProvider.CreateSharedAccessSignatureTokenProvider("RootManageSharedAccessKey", "Oo0sVn9SQkvuUQV3+TeWoPcpw59f/XTjU0gjuHv1++o=");
+            MessagingFactory factory = MessagingFactory.Create(serviceUri, credentials);
+            QueueClient client = factory.CreateQueueClient("messagesqueue");
+            BrokeredMessage msg = client.Receive();
+            Console.WriteLine("TID:"+msg.Properties["TID"]+"\t Amount:"+msg.Properties["Amt"]);
+            Console.Read();
+            msg.Complete();
+        }
+    }
+}
---
+++ queue messaging-servicebus/service/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Description;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceBus;
+
+namespace Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri serviceUri = ServiceBusEnvironment.CreateServiceUri("sb", "beccsea", String.Empty);
+            TokenProvider credentials = TokenProvider.CreateSharedAccessSignatureTokenProvider("RootManageSharedAccessKey", "Oo0sVn9SQkvuUQV3+TeWoPcpw59f/XTjU0gjuHv1++o=");
+            NamespaceManager nameSpaceManager = new NamespaceManager(serviceUri,credentials);
+            if(!nameSpaceManager.QueueExists("messagesqueue"))
+                nameSpaceManager.CreateQueue("messagesqueue");
+            MessagingFactory factory = MessagingFactory.Create(serviceUri, credentials);
+            QueueClient client = factory.CreateQueueClient("messagesqueue");
+            BrokeredMessage msg = new BrokeredMessage();
+            msg.Properties.Add("TID",100);
+            msg.Properties.Add("Amt",10000);
+            client.Send(msg);
+            Console.WriteLine("Message Added");
+            Console.Read();
+
+        }
+    }
+}
